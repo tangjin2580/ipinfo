79164774ce149aaabae1d3eab97521d2a6b5d0be
tangjin2580 <2324099478@qq.com>
2024-11-25 17:25:51 +0800

    安装脚本

--- start.sh
+++ start.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-cd /opt/ipinfo-main
+
+# 获取当前脚本所在的目录
+# source venv/bin/activate
+# pip install -r requirements.txt
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # 激活虚拟环境
-source venv/bin/activate
+source "$SCRIPT_DIR/venv/bin/activate" || { echo "虚拟环境不存在"; exit 1; }
 
-cd /opt/ipinfo-main/server/
+# 进入服务器目录
+cd "$SCRIPT_DIR/server" || { echo "无法进入 server 目录"; exit 1; }
+
 # 启动 Flask 应用程序使用 Gunicorn
 echo "启动 Flask 应用程序..."
 gunicorn app:app -b 0.0.0.0:8080 --workers 4 --pid gunicorn.pid &
--- stop.sh
+++ stop.sh
@@ -3,9 +3,27 @@
 # 读取 PID 文件并终止进程
 if [ -f gunicorn.pid ]; then
     PID=$(cat gunicorn.pid)
-    echo "关闭 Flask 应用程序 (PID: $PID)..."
-    kill $PID
-    rm gunicorn.pid  # 删除 PID 文件
+
+    # 检查进程是否存在
+    if ps -p "$PID" > /dev/null; then
+        echo "关闭 Flask 应用程序 (PID: $PID)..."
+        kill "$PID"
+
+        # 等待进程结束
+        if wait "$PID"; then
+            echo "Flask 应用程序已成功关闭。"
+        else
+            echo "关闭 Flask 应用程序失败，尝试强制终止..."
+            kill -9 "$PID" || { echo "强制终止失败，PID: $PID 不存在或无法关闭."; exit 1; }
+        fi
+
+        # 删除 PID 文件
+        rm -f gunicorn.pid
+        echo "已删除 PID 文件。"
+    else
+        echo "进程 $PID 不存在，无法关闭。"
+        rm -f gunicorn.pid  # 删除 PID 文件
+    fi
 else
     echo "没有找到运行的 Flask 应用程序。"
 fi
